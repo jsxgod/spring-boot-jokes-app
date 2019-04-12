90ccf219f9ee15da7dcf3c439ef5368a0d63504b
jsxgod <Smykoo7@gmail.com>
2019-04-13 01:31:54 +0200

    Refactoring to XML based configuration.

--- src/main/java/js/springexamples/jokesapp/JokesAppApplication.java
+++ src/main/java/js/springexamples/jokesapp/JokesAppApplication.java
@@ -2,8 +2,10 @@ package js.springexamples.jokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:joke-config.xml")
 public class JokesAppApplication {
 
     public static void main(String[] args) {
