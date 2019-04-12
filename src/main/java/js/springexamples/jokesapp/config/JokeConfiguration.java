923b134678e0b9d6d1227111df3f904b0a4f82ea
jsxgod <Smykoo7@gmail.com>
2019-04-13 01:23:55 +0200

    Refactoring to Java Configuration

---
+++ src/main/java/js/springexamples/jokesapp/config/JokeConfiguration.java
@@ -0,0 +1,14 @@
+package js.springexamples.jokesapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JokeConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+}
